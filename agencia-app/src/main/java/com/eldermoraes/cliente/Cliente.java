dd9a537fdcc6a971a65ba73aaad9f5c1a6983af8
Marcos Domingues <domingues.marcos@cwi.com.br>
2022-09-21 11:32:42 -0300

    WIP: Criação do projeto

---
+++ agencia-app/src/main/java/com/eldermoraes/cliente/Cliente.java
@@ -0,0 +1,10 @@
+package com.eldermoraes.cliente;
+
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Cliente extends PanacheEntity {
+    public String nome;
+}
